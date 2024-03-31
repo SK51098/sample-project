baaa393bf30c0ef6139d6f4f27662fbcff7fe8e6
Deepak271198 <deepakkumarganesan98@gmail.com>
2024-03-31 18:15:54 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import PageNotFound from "./pages/pageNotFound";
 import "./App.css";
@@ -14,8 +10,8 @@ const App = () => {
     <>
       <Router>
         <Routes>
-        <Route exact path="/" element={<Home />} />
-          <Route exact path="/" element={<FaqPage />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="faqpage" element={<FaqPage />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </Router>
